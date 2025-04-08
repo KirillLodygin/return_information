26fe5c87b590da996191d6992771aa62a6d41e59
Kirill Lodygin <vilyam13@gmail.com>
2025-04-08 10:30:38 +0300

    updated README

--- src/components/ProductInformation/index.vue
+++ src/components/ProductInformation/index.vue
@@ -1,110 +1,111 @@
 <template>
-  <div class="information-block">
-    <div class="information-block__container">
-      <div class="information-block__header">
-        <template v-if="isMaxDesktop">
-          <p class="information-block__header-line">Какой товар</p>
-          <p class="information-block__header-line">можно вернуть</p>
-        </template>
-        <p v-else class="information-block__header-line">Какой товар можно вернуть</p>
+  <div class="info-cell">
+    <div class="info-cell__content">
+      <div class="info-cell__number-line">
+        <div class="info-cell__number">{{ number }}</div>
+        <div class="info-cell__icon" aria-hidden="true"></div>
       </div>
-      <div class="information-block__cells">
-        <InfoCell
-            v-for="cell in GOODS"
-            :key="cell.number"
-            :number="cell.number"
-            :description="cell.description"
-            :term="cell.term"
-        />
-      </div>
+      <div class="info-cell__description">{{ description }}</div>
+      <div class="info-cell__term">{{ term }}</div>
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { GOODS } from '@/constants/projectInfo'
-import InfoCell from './InfoCell.vue'
+interface Props {
+  number: string
+  description: string
+  term: string
+}
 
-import useBreakpoints from '@/composables/useBreakpoints'
-
-const { isMaxDesktop } = useBreakpoints()
+defineProps<Props>()
 </script>
 
 <style scoped>
-.information-block {
-  width: 100%;
-  background-color: #ffffff;
-  padding: 112px 0;
+.info-cell {
+  width: min(282px, 100%);
+  border-radius: 12px;
+  padding: 16px;
+  background-color: #f0f9ff;
+  box-sizing: border-box;
 }
 
-.information-block__container {
-  max-width: 1200px;
-  width: 100%;
-  margin: auto;
+.info-cell__content {
   display: flex;
-  align-items: flex-start;
-  justify-items: flex-start;
-  gap: 24px;
+  flex-direction: column;
+  color: #0f172a;
+  height: 100%;
 }
 
-.information-block__header {
-  width: 282px;
-  color: #0f172a;
-  font-weight: 400;
-  font-size: 12px;
-  line-height: 100%;
+.info-cell__number-line {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 16px;
+  gap: 8px;
+}
+
+.info-cell__number {
+  font-weight: 500;
+  font-size: 2.5rem;
+  line-height: 1.1;
   letter-spacing: 0;
   text-transform: uppercase;
 }
 
-.information-block__header-line {
-  margin: 0;
+.info-cell__icon {
+  width: 30px;
+  height: 30px;
+  border-radius: 50%;
+  background-color: #ffffff;
+  background-image: url('@assets/images/card.jpg');
+  background-size: 55%;
+  background-position: center;
+  background-repeat: no-repeat;
+  flex-shrink: 0;
 }
 
-.information-block__cells {
-  display: flex;
-  align-items: flex-start;
-  justify-items: flex-start;
-  gap: 24px;
+.info-cell__description {
+  font-weight: 700;
+  font-size: 13px;
+  line-height: 1.2;
+  letter-spacing: 0;
+  text-transform: uppercase;
+  max-width: 180px;
+  margin-bottom: 40px;
 }
 
-@media (max-width: 1200px) {
-  .information-block__container {
-    flex-direction: column;
-    gap: 24px;
-    margin-left: 40px;
-  }
+.info-cell__term {
+  font-weight: 400;
+  font-size:15px;
+  line-height: 1.1;
+  letter-spacing: 0;
+  max-width: 250px;
 }
 
 @media (max-width: 900px) {
-  .information-block {
-    padding: 80px 0;
-  }
-
-  .information-block__header {
-    width: 100%;
-  }
-
-  .information-block__container {
-    margin: auto;
+  .info-cell {
     width: 80%;
+    max-width: 400px;
   }
 
-  .information-block__cells {
-    flex-direction: column;
-    gap: 25px;
-    width: 100%;
+  .info-cell__description {
+    max-width: 80%;
+    margin-bottom: 2rem;
+  }
+
+  .info-cell__term {
+    max-width: 100%;
   }
 }
 
 @media (max-width: 640px) {
-  .information-block {
-    padding: 60px 0;
+  .info-cell {
+    width: 100%;
   }
 
-  .information-block__header {
-    font-size: 11px;
+  .info-cell__description {
+    margin-bottom: 1.5rem;
   }
 }
 </style>
-
